c386961985b9cda9d3e275bed8f7f29ab541d512
Majid Alshammari <MRF@Majids-MacBook-Pro-2.local>
2017-11-16 22:47:45 -0500

    add the fourth line

---
+++ terminal.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "test terminal!\n";
+  std::cout << "/* just new */" << '\n';
+
+  int a = 500;
+  int b = 1000;
+
+  int c = a * b;`1FV`
+
+  std::cout << c << '\n';
+  std::cout << " addin a newthing" << '\n';
+  std::cout << "/* message */" << '\n';
+  std::cout <<"check the commit" << '\n';
